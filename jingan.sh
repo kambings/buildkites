abbb8f7b6b98c28cdbe4db72502a0bf51c7cb06b
asamjawas <85292358+asamjawas@users.noreply.github.com>
2021-06-28 05:16:45 +0700

    Update jingan.sh

--- jingan.sh
+++ jingan.sh
@@ -2,7 +2,7 @@
 apt-get update -y
 COUNTER=50
 until [  $COUNTER -lt 10 ]; do
-TOKEN="5250c6f21ceab78e14a2a0b822a32f7ec69f4477808984816a" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="0eb5937706479e2f640f47058dead40dc960e14671d6e5ea21" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 timeout 288m ~/.buildkite-agent/bin/buildkite-agent start
 echo "yohoho"
 
